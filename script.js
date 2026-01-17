2799312946f1b6e7226e584c8b287c8a43198f85
ilikemelons21 <toni.filipovic555+1@gmail.com>
2026-01-17 11:00:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const buttons = document.querySelectorAll(".menu-btn");
 
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    button.classList.add("clicked");
+
+    setTimeout(() => {
+      button.classList.remove("clicked");
+    }, 200);
+  });
+});
