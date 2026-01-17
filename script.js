9f0521f8f2361b109570c30c64684451f9806d7b
ilikemelons21 <toni.filipovic555+1@gmail.com>
2026-01-17 11:49:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-function openPage(id) {
-  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+function openPage(id){
+  document.querySelectorAll(".page").forEach(p=>p.classList.remove("active"));
   document.getElementById(id).classList.add("active");
 }
-
-function goHome() { openPage("home"); }
-
-function copyUser(username) {
+function goHome(){ openPage("home"); }
+function copyUser(username){
   navigator.clipboard.writeText(username);
-  alert("Kopirano: " + username);
+  alert("Kopirano: "+username);
 }
